0bcf03fa2c6c8059e8d565668d985b8706f91489
shibayamasho <men.in.boss2@gmail.com>
2022-10-24 12:57:38 +0900

    PostListクラス生成

---
+++ lib/widgets/post_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PostList extends StatelessWidget {
+  const PostList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
