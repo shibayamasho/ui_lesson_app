2a4c8489fd629261bb6d1f2b1339d0ee29574e7d
shibayamasho <men.in.boss2@gmail.com>
2022-10-24 12:32:02 +0900

    SideNavigationクラス生成

---
+++ lib/widgets/side_navigation_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SideNavigation extends StatefulWidget {
+  const SideNavigation({super.key});
+
+  @override
+  State<SideNavigation> createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
