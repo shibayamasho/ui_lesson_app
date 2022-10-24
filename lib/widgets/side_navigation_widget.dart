35d09059babfe284ed6878b5af0f50395f3e6da3
shibayamasho <men.in.boss2@gmail.com>
2022-10-24 12:37:14 +0900

    SideNavigation追記

--- lib/widgets/side_navigation_widget.dart
+++ lib/widgets/side_navigation_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,8 +10,35 @@ class SideNavigation extends StatefulWidget {
 }
 
 class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
+    );
   }
 }
